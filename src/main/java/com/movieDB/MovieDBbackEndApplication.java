15978d79aafb3f344467054556476fd975352998
subha2592 <bhowmik0205@gmail.com>
2021-05-27 13:14:51 +0600

    first commit

---
+++ src/main/java/com/movieDB/MovieDBbackEndApplication.java
@@ -0,0 +1,13 @@
+package com.movieDB;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MovieDBbackEndApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MovieDBbackEndApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/movieDB/MovieDBbackEndApplicationTests.java
@@ -0,0 +1,13 @@
+package com.movieDB;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MovieDBbackEndApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
